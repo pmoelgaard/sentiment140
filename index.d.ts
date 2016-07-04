0951118d4a1afb4f7b8d2d0695b2175ec1a474d0
Peter Andreas Moelgaard <admin@petermolgaard.com>
2016-07-04 12:46:52 +0800

    ts error fixed, and ts added to npmignore

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 ///<reference path="typings/index.d.ts" />
 
 export interface ISentimentAnalyzer {
-    sentiment(data:any|Array<any>, callback?:Function) : Promise;
+    sentiment(data:any|Array<any>, callback?:Function) : Promise<any>;
 }
